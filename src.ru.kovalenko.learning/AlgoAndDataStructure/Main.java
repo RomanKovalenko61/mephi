b3f47edc51fafbaa3b1f67bc314b7b5b2b1348c8
Roman <astek14@mail.ru>
2024-12-05 20:30:49 +0300

    add test methods with comparator

--- src.ru.kovalenko.learning/AlgoAndDataStructure/Main.java
+++ src.ru.kovalenko.learning/AlgoAndDataStructure/Main.java
@@ -1,6 +1,7 @@
 package AlgoAndDataStructure;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,9 +15,9 @@ public class Main {
         int[] testArr = {0, 4, 5, 8, 11, 12, 14, 17, 19, 21};
         System.out.println("Тестирование бинарного поиска для массива int");
         // Ищем число, которое есть в массиве
-        getInfo(testArr, 8);
+        printInfo(testArr, 8);
         // Ищем число, которого нет в массиве
-        getInfo(testArr, 15);
+        printInfo(testArr, 15);
 
         System.out.println("Тестирование бинарного поиска для коллекций");
 
@@ -37,14 +38,42 @@ public class Main {
         linkListTest.add(test12);
 
         System.out.println("Поиск в коллекций имплементирующей RandomAccess (ArrayList)");
-        getInfo(arrListTest, new Test(2, "test"));
-        getInfo(arrListTest, new Test(4, "test"));
+        printInfo(arrListTest, new Test(2, "test"));
+        printInfo(arrListTest, new Test(4, "test"));
         System.out.println("Поиск в коллекций не имплементирующей RandomAccess (LinkedList)");
-        getInfo(linkListTest, new Test(8, "test"));
-        getInfo(linkListTest, new Test(15, "test"));
+        printInfo(linkListTest, new Test(8, "test"));
+        printInfo(linkListTest, new Test(15, "test"));
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Теперь передаем отдельно компаратор");
+
+        TestWithoutComp t1 = new TestWithoutComp(1, "Roman");
+        TestWithoutComp t2 = new TestWithoutComp(2, "Kseniya");
+        TestWithoutComp t5 = new TestWithoutComp(5, "Dmitriy");
+        List<TestWithoutComp> arrListTestWithoutComp = new ArrayList<>();
+        arrListTestWithoutComp.add(t1);
+        arrListTestWithoutComp.add(t2);
+        arrListTestWithoutComp.add(t5);
+
+        TestWithoutComp t8 = new TestWithoutComp(8, "Alexandra");
+        TestWithoutComp t9 = new TestWithoutComp(9, "Artem");
+        TestWithoutComp t12 = new TestWithoutComp(12, "Elena");
+        List<TestWithoutComp> linkListTestWithoutComp = new LinkedList<>();
+        linkListTestWithoutComp.add(t8);
+        linkListTestWithoutComp.add(t9);
+        linkListTestWithoutComp.add(t12);
+
+        System.out.println("Поиск в коллекций имплементирующей RandomAccess (ArrayList) + Comparator");
+        arrListTestWithoutComp.sort((o1, o2) -> o1.getName().compareTo(o2.name));
+        printInfo(arrListTestWithoutComp, new TestWithoutComp(1, "Roman"), (o1, o2) -> o1.getName().compareTo(o2.name));
+        printInfo(arrListTestWithoutComp, new TestWithoutComp(1, "Alex"), (o1, o2) -> o1.getName().compareTo(o2.name));
+        System.out.println("Поиск в коллекций не имплементирующей RandomAccess (LinkedList) + Comparator");
+        linkListTestWithoutComp.sort((o1, o2) -> o1.getName().compareTo(o2.name));
+        printInfo(linkListTestWithoutComp, new TestWithoutComp(1, "Alexandra"), (o1, o2) -> o1.getName().compareTo(o2.name));
+        printInfo(linkListTestWithoutComp, new TestWithoutComp(1, "Penelopa"), (o1, o2) -> o1.getName().compareTo(o2.name));
     }
 
-    private static void getInfo(int[] arr, int key) {
+    private static void printInfo(int[] arr, int key) {
         System.out.println("Ищем по ключу в массиве " + java.util.Arrays.toString(arr));
         int index = Arrays.binarySearch(arr, key);
         if (index >= 0) {
@@ -57,13 +86,23 @@ public class Main {
         }
     }
 
-    private static <T> void getInfo(List<? extends Comparable<? super T>> list, T key) {
+    private static <T> void printInfo(List<? extends Comparable<? super T>> list, T key) {
         System.out.println("Ищем по ключу id в коллекции " + list);
         int index = Collections.binarySearch(list, key);
+        printAdditional(index, list, key);
+    }
+
+    private static <T> void printInfo(List<? extends T> list, T key, Comparator<? super T> c) {
+        System.out.println("Ищем по ключу id в коллекции " + list);
+        int index = Collections.binarySearch(list, key, c);
+        printAdditional(index, list, key);
+    }
+
+    private static <T> void printAdditional(int index, List<?> list, T key) {
         if (index >= 0) {
-            System.out.println("Элемент c id " + ((Test) key).getId() + " найден по индексу " + index);
+            System.out.println("Элемент c id " + key + " найден по индексу " + index);
         } else {
-            System.out.println("Элемент с id " + ((Test) key).getId() +
+            System.out.println("Элемент с id " + key +
                     " не найден, но его можно вставить по индексу " + (-index - 1));
             if ((-index - 1) == list.size()) {
                 System.out.println("Можно добавить в конец коллекции");
@@ -71,6 +110,32 @@ public class Main {
                 System.out.println("Вставка элемента в коллекцию сместит значения после него на одну позицию вправо");
             }
         }
+    }
+}
+
+class TestWithoutComp {
+    Integer id;
+    String name;
+
+    public TestWithoutComp(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "TestWithoutComp{" +
+                "id=" + id +
+                ", name=" + name +
+                '}';
     }
 }
 
